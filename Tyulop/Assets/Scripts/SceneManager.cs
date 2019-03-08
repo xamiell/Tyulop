dce927f91ad2d632de322c92f7d7910b6d202741
Elias Pujols <eliasnurbs@gmail.com>
2019-03-08 10:59:41 -0400

    Added Sounds and Music Background

--- Tyulop/Assets/Scripts/SceneManager.cs
+++ Tyulop/Assets/Scripts/SceneManager.cs
@@ -6,14 +6,39 @@ using UnityEngine.SceneManagement;
 public class SceneManager : MonoBehaviour
 {
     [SerializeField] string nextSceneName;
+    [SerializeField] AudioClip audioClick;
+    [SerializeField] AudioSource musicBackground;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(this);
+        DontDestroyOnLoad(musicBackground);
+    }
 
     public void OnLoadScene()
     {
+        PlayClickSound();
+
+        StartCoroutine(nameof(WaitBeforeExecuteMethod));
+
         UnityEngine.SceneManagement.SceneManager.LoadScene(nextSceneName);
+    }
+
+    IEnumerator WaitBeforeExecuteMethod()
+    {
+        yield return new WaitForSeconds(1.5f);
+    }
+
+    private void PlayClickSound()
+    {
+        AudioSource.PlayClipAtPoint(audioClick, Camera.main.transform.position);
     }
 
     public void OnExitGame()
     {
+        PlayClickSound();
+        StartCoroutine(WaitBeforeExecuteMethod());
+
         Application.Quit();
     }
 }
