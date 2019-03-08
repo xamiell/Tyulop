4aec74bb64817a8b847f3092c83502b23e7e2e08
Elias Pujols <eliasnurbs@gmail.com>
2019-03-08 09:46:39 -0400

    Scenes Loadable
    Added arena scene and clickeable buttons to load scenes

---
+++ Tyulop/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    [SerializeField] string nextSceneName;
+
+    public void OnLoadScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(nextSceneName);
+    }
+
+    public void OnExitGame()
+    {
+        Application.Quit();
+    }
+}
