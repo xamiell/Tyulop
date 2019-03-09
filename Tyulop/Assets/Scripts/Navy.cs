fb8c156a2eefd21a3a840ae5d695ccf109e86d13
Elias Pujols <eliasnurbs@gmail.com>
2019-03-09 11:15:57 -0400

    Fairy sprite and animation created
    Animation that fairy move around the player

---
+++ Tyulop/Assets/Scripts/Navy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Navy : MonoBehaviour
+{
+    [SerializeField] List<Transform> wayPoints;
+
+    [Range(5, 20)]
+    [SerializeField] float speedMovement;
+
+    private int moveCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.transform.position = wayPoints[moveCount].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (moveCount <= wayPoints.Count)
+        {
+            var targetPosition = wayPoints[moveCount].transform.position;
+            var newPosition = speedMovement * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, newPosition);
+
+            if (targetPosition == transform.position)
+            {
+                moveCount++;
+            }
+        }
+
+        if(moveCount >= wayPoints.Count)
+        {
+            moveCount = 0;
+        }
+    }
+}
